c96abfeb917ac883470aa057dcdb0cc6ce4aeea0
Aalfinuraziz <alfinuraziz885@gmail.com>
2021-01-15 21:36:33 +0700

    Add project files.

---
+++ ServerChatApp_034_AlfiNurAziz/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCFService_2Way_034;
+
+namespace ServerChatApp_034_AlfiNurAziz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+            try
+            {
+                hostObjek = new ServiceHost(typeof(ServiceCallback));
+                hostObjek.Open();
+
+                Console.WriteLine("Server Ready");
+                Console.ReadLine();
+
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
